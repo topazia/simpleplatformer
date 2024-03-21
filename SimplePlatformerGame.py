bac3bad9aa68c5945f50450f6cb8ce4378f34cb3
topaz <145774978+topazia@users.noreply.github.com>
2024-03-21 10:51:03 +0000

    SimplePlatformerGame v0.0.2

--- SimplePlatformerGame.py
+++ SimplePlatformerGame.py
@@ -1,1 +1,8 @@
-import pygame
+# # # Simple Platformer Game
+print("Simple Platformer Game, version 0.0.2")
+# imported modules
+import pygame, math, turtle, numpy
+# resolution
+HEIGHT = 800
+WIDTH = 600
+BACKGROUND = (0,0,0)
